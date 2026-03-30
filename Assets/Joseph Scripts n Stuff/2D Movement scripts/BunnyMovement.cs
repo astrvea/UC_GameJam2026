987d4dd2150b1842dd8806c10b7a2254da75b58d
WUMPERNUGGET <rjwilley1129@gmail.com>
2026-03-30 03:59:19 -0400

    Completed 2D movement
    should all be adjustable and stuff for whatever we need

--- Assets/Joseph Scripts n Stuff/2D Movement scripts/BunnyMovement.cs
+++ Assets/Joseph Scripts n Stuff/2D Movement scripts/BunnyMovement.cs
@@ -17,12 +17,38 @@ public class BunnyMovement : MonoBehaviour
 
     public float targetSpeed;
 
+    public bool canJump;
+    public bool isJumping;
+    public float jumpTime;
+    //public float jumpTimeReset;
+
+    public float jumpReset;
+    public float jumpForce;
+    public float jumpCutForce;
+
+    public float coyoteTime;
+    public float coyoteTimeReset;
+
+    public BoxCollider groundCheck;
+
+    public LayerMask groundLayer;
+
+    public ShadowTime st;
+
+
+
 
     // To Do : Jump, jump cut, coyote time, 
 
 
     public void Awake()
     {
+
+        canJump = true;
+
+        jumpReset = jumpTime;
+
+        coyoteTimeReset = coyoteTime;
 
 
     }
@@ -34,49 +60,123 @@ public class BunnyMovement : MonoBehaviour
 
 
 
-
-
-
-
-        brb.velocity = new Vector3(0, brb.velocity.y, -moveInput.x * moveSpeed);
-
-        if (brb.velocity.z > 0)
+        if (st.ShadowGo == true) 
         {
-            brb.AddForce(0, 0, accelRate);
-        }
+                brb.velocity = new Vector3(0, brb.velocity.y, -moveInput.x * moveSpeed);
 
-        if (brb.velocity.z < 0) 
+              if (brb.velocity.z > 0)
+              {
+                brb.AddForce(0, 0, accelRate);
+              }
+
+              if (brb.velocity.z < 0) 
         
-        {
-            brb.AddForce(0, 0, -accelRate);
+              {
+                    brb.AddForce(0, 0, -accelRate);
+              }
+
+            if (brb.velocity.z > 0f && brb.velocity.z < 1f) 
+            {
+                brb.AddForce(0, 0, -decelRate);
+            }
+
+            if (brb.velocity.z < 0f && brb.velocity.z > -1f) 
+            {
+                brb.AddForce(0, 0, decelRate);
+            }
+
+
+
         }
 
-        if (brb.velocity.z > 0f && brb.velocity.z < 1f) 
+        if (isJumping == true) 
         {
-            brb.AddForce(0, 0, -decelRate);
+            jumpTime -= Time.deltaTime;
         }
 
-        if (brb.velocity.z < 0f && brb.velocity.z > -1f) 
-        {
-            brb.AddForce(0, 0, decelRate);
-        }
 
+        
 
 
 
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
 
-        moveInput.x = Input.GetAxis("Horizontal");
+        if (st.ShadowGo == true) 
+        {
+            moveInput.x = Input.GetAxis("Horizontal");
+
+            if (Input.GetKeyDown("space") && canJump == true && coyoteTime > 0) 
+            {
+
+                brb.AddForce(0, jumpForce, 0);
+                isJumping = true;
+                canJump = false;
+
+                //jumpTime -= Time.deltaTime;
+                //StartCoroutine(JumpTimer());
+
+            }
+
+            if (Input.GetKeyUp("space") && canJump == false)
+            {
+
+                isJumping = false;
+                jumpTime = 0f;
+                //StopCoroutine(JumpTimer());
+
+            }
+
+
+
+            if (isJumping == false && canJump == false) 
+            {
+
+            brb.AddForce(0, -jumpCutForce, 0);
+            
+            }
+
+            if (jumpTime <= 0f) 
+            {
+                isJumping = false;
+            }
+            
+
+        }
+
+        if (canJump == true) 
+        {
+            //StopCoroutine(JumpTimer());
+            //jumpTime = jumpTimeReset;
+        }
+
+            
+        
+        
+        
         
     }
+
+    //IEnumerator JumpTimer() 
+    //{
+        //yield return new WaitForSeconds(jumpTime);
+        //isJumping = false;
+    //
+
+    private void OnTriggerEnter(Collider col) 
+    {
+
+        if (col.CompareTag("shadowFloor")) 
+        {
+            canJump = true;
+        }
+
+        
+
+    }
+
+
 }
---
+++ Assets/Joseph Scripts n Stuff/Camera Scripts/GroundChecking.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecking : MonoBehaviour
+{
+
+    public GameObject groundCheck;
+
+    public BunnyMovement bm;
+
+    public bool startCoyotetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (startCoyotetime == true) 
+        {
+            bm.coyoteTime -= Time.deltaTime;
+        }
+
+        if (startCoyotetime == false) 
+        {
+            bm.coyoteTime = bm.coyoteTimeReset;
+        }
+
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("shadowFloor")) 
+        {
+            bm.canJump = true;
+            bm.jumpTime = bm.jumpReset;
+            bm.coyoteTime = bm.coyoteTimeReset;
+            startCoyotetime = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("shadowFloor")) 
+        {
+            startCoyotetime = true;
+        }
+    }
+}
